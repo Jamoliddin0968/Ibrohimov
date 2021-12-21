e841a9c66dcd468bf06987179ce85b7ba496eb0e
Jamoliddin Ibrohimov <jamoliddin0968@gmail.com>
2021-12-21 09:30:44 -0800

    Update models.py

--- products/models.py
+++ products/models.py
@@ -4,3 +4,6 @@ class Mahsulot(models.Model):
 	name = models.TextField(max_length=200)
 	image = models.ImageField(upload_to='images',blank = True)
 	cost = models.IntegerField(blank = True)
+
+	def __str__(self):
+		return self.name
